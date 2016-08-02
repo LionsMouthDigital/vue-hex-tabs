6b765459aa8e8dd4367d2d874ae44c13b27e3501
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-08-02 15:53:48 -0500

    :stars: allow populating with JSON

--- src/components/HexTabList.vue
+++ src/components/HexTabList.vue
@@ -1,5 +1,34 @@
 <template>
-  <ol class="tab-list" role="tablist">
+  <ol class="tab-list" role="tablist" v-if="json.length">
+    <hex-tab v-for="item in jsonData">
+      {{{ item.markup }}}
+    </hex-tab>
+  </ol>
+
+  <ol class="tab-list" role="tablist" v-else>
     <slot></slot>
   </ol>
 </template>
+
+<script>
+  var HexTab = require('./HexTab.vue');
+
+  export default {
+    components: {
+      HexTab,
+    },
+
+    props: {
+      json:   {
+        type:    String,
+        default: '',
+      },
+    },
+
+    computed: {
+      jsonData() {
+        return this.json.length ? JSON.parse(this.json.replace(/\n/g, '')) : '';
+      }
+    },
+  }
+</script>
--- src/components/HexTabPanelList.vue
+++ src/components/HexTabPanelList.vue
@@ -1,14 +1,36 @@
 <template>
-  <ol class="tab-panel-list">
+  <ol class="tab-panel-list" v-if="json.length">
+    <hex-tab-panel v-for="item in jsonData">
+      {{{ item.markup }}}
+    </hex-tab-panel>
+  </ol>
+
+  <ol class="tab-panel-list" v-else>
     <slot></slot>
   </ol>
 </template>
 
 <script>
+  var HexTabPanel = require('./HexTabPanel.vue');
+
   export default {
+    components: {
+      HexTabPanel,
+    },
+
     props: {
       // This gets used for `:transition` in `tab-panel.vue`.
       effect: String,
+      json:   {
+        type:    String,
+        default: '',
+      },
+    },
+
+    computed: {
+      jsonData() {
+        return this.json.length ? JSON.parse(this.json.replace(/\n/g, '')) : '';
+      }
     },
   }
 </script>
