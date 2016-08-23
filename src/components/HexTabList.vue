3f6fbfeda5ca2765a8628194617bdfb0887a1c9e
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-08-23 11:41:07 -0500

    Revert ":stars: allow populating with JSON"
    This reverts commit 6b765459aa8e8dd4367d2d874ae44c13b27e3501.

--- src/components/HexTabList.vue
+++ src/components/HexTabList.vue
@@ -1,34 +1,5 @@
 <template>
-  <ol class="tab-list" role="tablist" v-if="json.length">
-    <hex-tab v-for="item in jsonData">
-      {{{ item.markup }}}
-    </hex-tab>
-  </ol>
-
-  <ol class="tab-list" role="tablist" v-else>
+  <ol class="tab-list" role="tablist">
     <slot></slot>
   </ol>
 </template>
-
-<script>
-  var HexTab = require('./HexTab.vue');
-
-  export default {
-    components: {
-      HexTab,
-    },
-
-    props: {
-      json:   {
-        type:    String,
-        default: '',
-      },
-    },
-
-    computed: {
-      jsonData() {
-        return this.json.length ? JSON.parse(this.json.replace(/\n/g, '')) : '';
-      }
-    },
-  }
-</script>
--- src/components/HexTabPanelList.vue
+++ src/components/HexTabPanelList.vue
@@ -1,36 +1,14 @@
 <template>
-  <ol class="tab-panel-list" v-if="json.length">
-    <hex-tab-panel v-for="item in jsonData">
-      {{{ item.markup }}}
-    </hex-tab-panel>
-  </ol>
-
-  <ol class="tab-panel-list" v-else>
+  <ol class="tab-panel-list">
     <slot></slot>
   </ol>
 </template>
 
 <script>
-  var HexTabPanel = require('./HexTabPanel.vue');
-
   export default {
-    components: {
-      HexTabPanel,
-    },
-
     props: {
       // This gets used for `:transition` in `tab-panel.vue`.
       effect: String,
-      json:   {
-        type:    String,
-        default: '',
-      },
-    },
-
-    computed: {
-      jsonData() {
-        return this.json.length ? JSON.parse(this.json.replace(/\n/g, '')) : '';
-      }
     },
   }
 </script>
