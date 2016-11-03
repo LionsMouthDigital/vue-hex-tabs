04766ac552e5af8021da5dd09cf37430b52c0a2d
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-11-03 16:07:50 -0500

    :sparkles: slightly cleaner

---
+++ src/HexTabs.js
@@ -0,0 +1,13 @@
+import HexTabs        from './components/HexTabs.vue';
+import HexTab         from './components/HexTab.vue';
+import HexTabPanels   from './components/HexTabPanels.vue';
+import HexTabPanel    from './components/HexTabPanel.vue';
+import HexTabControls from './components/HexTabControls.vue';
+
+export default {
+  HexTabs,
+  HexTab,
+  HexTabPanels,
+  HexTabPanel,
+  HexTabControls,
+};
